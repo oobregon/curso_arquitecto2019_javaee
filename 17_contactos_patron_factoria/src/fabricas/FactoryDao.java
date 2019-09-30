5b28827f0eb50d873bd316dc3bc7e864ebd47a59
tarde <tarde@G24-EQ09>
2019-09-30 20:10:38 +0200

    Clase Factory modificada

--- 17_contactos_patron_factoria/src/fabricas/FactoryDao.java
+++ 17_contactos_patron_factoria/src/fabricas/FactoryDao.java
@@ -4,6 +4,7 @@ import daos.DaoContactos;
 import daos.DaoContactosImpl;
 
 // Implementa el patrón factory
+// Fábrica modificada
 public class FactoryDao {
 	public static DaoContactos obtenerDaoContactos() {
 		return new DaoContactosImpl();
